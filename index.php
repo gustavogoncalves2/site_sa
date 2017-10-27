dd4f0f9fa067c10793688a83626afce079856322
gustavo_goncalves2 <gustavo_goncalves2@SNRSL1ELBIET035>
2017-10-27 08:23:45 -0200

    Versão Inicial do Projeto

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <div class="container">
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" href="css/estilo.css" type="text/css"/>
+            <title>GR7 Informática</title>
+        </head>
+        <body>
+            <header>
+                <img src="img/banner.png" alt="Banner" class="banner">
+            </header>
+            <div id="menu">
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        Programa de descarte
+                    </li>
+                    <li>
+                        Equipe
+                    </li>
+                    <li>
+                       Produtos
+                    </li>
+                    <li>
+                        Serviços
+                    </li>
+                    <li>
+                        Empresa
+                    </li>
+                    <li>
+                        FAQ
+                    </li>
+                </ul>
+            </div>
+    </div>
+    <?php
+    // put your code here
+    ?>
+</body>
+</html>
